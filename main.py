38b3523f72f809a5f732a070f61efca7a36b8b12
Janhvi Singh <singhjanhvi1717@example.com>
2026-06-14 13:12:16 +0530

    Add inventory menu

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 #store inventory system naviagtion menu
 while True :
-    print("\033[1;32;40m Store Inventory System \033[0m\n")
-    print("\033[1;32;40m Choose your desired option :  \033[0m\n")
+    print("\033[1;32;40m     Store Inventory System      \033[0m")
+    print("\033[1;32;40m Choose your desired option : \033[0m")
     print ("1.Product Managment \n2.Stock Management \n3.Order Management \n4.Stock Management \n5.Exit")
     user_choice=int(input("Enter your desired option :"))
     if user_choice ==1 :
-        print("Product Managment Selected ...")
+        print("\033[0;33;40m Product Managment Selected ...\033[0m")
     elif user_choice ==2 :
-        print("Stock Management Selected ...")
+        print("\033[0;33;40m Stock Management Selected ...\033[0m")
     elif user_choice ==3 :
-        print("Order Management Selected ...")
+        print("\033[0;33;40m Order Management Selected ...\033[0m")
     elif user_choice ==4 :
-        print("Stock Management Selected ...")
+        print("\033[0;33;40m Stock Management Selected ...\033[0m")
     elif user_choice ==5 :
-        print("Exiting the program ...")
+        print("\033[1;31;40m Exiting the program ...\033[0m")
         break
     else :
         print("Invalid Choice, Try Again !!!")
