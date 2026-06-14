93bdcf1f2cf0c6b5d63ef865bfee7b05ab9d5748
Janhvi Singh <singhjanhvi1717@example.com>
2026-06-14 14:00:11 +0530

    solved bugs and added excpetion block

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 #store inventory system naviagtion menu
-while True :
-    print("\033[1;32;40m     Store Inventory System      \033[0m")
+def main_menu():
+    print ("1.Product Managment \n2.Stock Management \n3.Order Management \n4.Sales Management \n5.Exit")
+print("\033[1;32;40m     Store Inventory System      \033[0m")
+while True:
     print("\033[1;32;40m Choose your desired option : \033[0m")
-    print ("1.Product Managment \n2.Stock Management \n3.Order Management \n4.Stock Management \n5.Exit")
-    user_choice=int(input("Enter your desired option :"))
-    if user_choice ==1 :
+    main_menu()
+    try:
+        user_choice=int(input("Enter your desired option :"))
+    except ValueError:
+        print("Invalid Input, Try Entering a Number (integer value form 1 to 5 )")
+        continue
+    if user_choice ==1:
         print("\033[0;33;40m Product Managment Selected ...\033[0m")
-    elif user_choice ==2 :
+    elif user_choice ==2:
         print("\033[0;33;40m Stock Management Selected ...\033[0m")
-    elif user_choice ==3 :
+    elif user_choice ==3:
         print("\033[0;33;40m Order Management Selected ...\033[0m")
-    elif user_choice ==4 :
-        print("\033[0;33;40m Stock Management Selected ...\033[0m")
-    elif user_choice ==5 :
+    elif user_choice ==4:
+        print("\033[0;33;40m Sales Management Selected ...\033[0m")
+    elif user_choice ==5:
         print("\033[1;31;40m Exiting the program ...\033[0m")
         break
     else :
